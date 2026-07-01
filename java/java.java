46bb5374ef14eec5c7778eddde9ce5d5c6448835
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-07-01 09:17:04 +0530

    modified:   fee/FEE.html

---
+++ java/java.java
@@ -0,0 +1,36 @@
+public class java {
+    public static void main(String[] args) {
+
+        // int age = 18;
+
+        // if (age == 18) {
+        //     System.out.println("adult");
+        // } else {
+        //     System.out.println("child");
+        // }
+   
+        int n = 5;
+    // square 
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+    // inverted triangle
+        // for (int i = n; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+    // right triangle
+        // for (int i = 0; i<=n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+}
+}
