37f37313f4e533c06cb2709ce56d5119d3dfa171
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-07-22 10:17:10 +0530

    modified:   fee/FEE.html

---
+++ java/tempCodeRunnerFile.java
@@ -0,0 +1,13 @@
+
+    public void Username(String username) {
+        this.username = username;
+    }
+    public void AccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+    public void AmountWithd(double amountWithdraw) {
+        this.amountWithdraw = amountWithdraw;
+    }
+    public void AmountCred(double amountCredit) {
+        this.amountCredit = amountCredit;
+    }
