46b1927c942ee3bfbf48b843b1c7831e4e6a127b
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-08-10 14:07:47 +0530

    modified:   SDE/sde.py

--- SDE/sde.py
+++ SDE/sde.py
@@ -2,4 +2,13 @@ import pandas as pd
 
 df = pd.read_excel("student.xlsx")
 
-print(df)
+# print(df)
+# print(df.shape)
+# print(df.columns)
+# print(df.dtypes)
+# print(df.info())
+# print(df.describe())
+# print(df.isnull().sum()/len(df)*100)
+
+# print(df.dropna())
+print(df.fillna(df.mean(numeric_only=True),inplace=True))
