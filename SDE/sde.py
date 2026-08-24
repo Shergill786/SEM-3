7ab0796f988022e1a556e49d5c1ca2b5788c4fb3
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-08-24 11:43:32 +0530

    modified:   SDE/sde.py

--- SDE/sde.py
+++ SDE/sde.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_excel("student.xlsx")
+df = pd.read_excel("SDE/Diabetes_data.xlsx")
 
 # print(df)
 # print(df.shape)
@@ -11,4 +11,16 @@ df = pd.read_excel("student.xlsx")
 # print(df.isnull().sum()/len(df)*100)
 
 # print(df.dropna())
-print(df.fillna(df.mean(numeric_only=True),inplace=True))
+
+# df2 = df.fillna(df['Age'].mean())
+# df2=df.fillna(df.mean(numeric_only=True), inplace=True)
+
+# print(df[df['Marks1']<90])  
+# print(df[df['Group']=='G3'])
+
+# print(df.columns)
+# df2=df[['Name','Roll No','Marks Obtained','Group.']]
+# print(df2['Group.'].value_counts())
+
+print(df)
+
