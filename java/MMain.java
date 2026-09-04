4f1ce70e57ec395c937c1bce79a12df71a1228d8
Jaskirat Singh <jaskirat3426.beai25@chitkara.edu.in>
2026-09-04 18:52:32 +0530

    modified:   java/MMain.class

--- java/MMain.java
+++ java/MMain.java
@@ -1,56 +1,17 @@
-import java.util.Scanner;
-
-// Custom Exception
-class InvalidMarksException extends Exception {
-
-    public InvalidMarksException(String message) {
-        super(message);
-    }
-}
-
-// Student Class
-class Student {
-    private int marks;
-    public Student(int marks) {
-        this.marks = marks;
-    }
-
-    public void checkMarks() throws InvalidMarksException {
-
-        if (marks < 0 || marks > 100) {
-            throw new InvalidMarksException(
-                "Marks must be between 0 and 100"
-            );
-        }
-
-        System.out.println("Valid marks: " + marks);
-    }
-}
-
-public class Main {
-
+public class MMain {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        int marks = sc.nextInt();
-
-    
-        Student student = new Student(marks);
-
         try {
+            int age = 15;
 
-            student.checkMarks();
+            if (age < 18) {
+                throw new IllegalArgumentException("Not eligible");
+            }
 
-        } catch (InvalidMarksException e) {
+            System.out.println("Eligible");
 
-            System.out.println("Error: " + e.getMessage());
-
-        } finally {
-
-            System.out.println("Marks checking completed");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-
-        sc.close();
     }
 }
